857c5ca56ef637365d7db5a1fb58236896c1e3ae
albert306 <a.wolf04@protonmail.com>
2024-07-08 15:50:05 +0200

    added first classes for networking functionality

---
+++ shared/src/commonMain/kotlin/networking/HttpRoutes.kt
@@ -0,0 +1,8 @@
+package networking
+
+object HttpRoutes {
+    private const val BASE_URL = "https://webapi.vvo-online.de"
+    const val STOP_FINDER = "$BASE_URL/tr/pointfinder"
+    const val STOP_MONITOR = "$BASE_URL/dm"
+    const val STOP_MONITOR_STOPSCHEDULE = "$BASE_URL/dm/trip"
+}
