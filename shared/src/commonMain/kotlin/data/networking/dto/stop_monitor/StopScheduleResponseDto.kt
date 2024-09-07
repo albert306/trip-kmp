b5fb894e1a95e6cc7b53a0d69f4941daa3bc25ba
albert306 <a.wolf04@protonmail.com>
2024-09-07 20:13:00 +0200

    removed unused import

--- shared/src/commonMain/kotlin/data/networking/dto/stop_monitor/StopScheduleResponseDto.kt
+++ shared/src/commonMain/kotlin/data/networking/dto/stop_monitor/StopScheduleResponseDto.kt
@@ -1,7 +1,6 @@
 package data.networking.dto.stop_monitor
 
 import data.networking.dto.ResponseStatusDto
-import data.networking.dto.stop_monitor.StopScheduleItemDto
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
