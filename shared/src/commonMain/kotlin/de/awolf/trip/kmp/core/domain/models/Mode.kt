6d6db69c2ebaa088d0be3f9036ac5e4c35f0cdc3
albert306 <a.wolf04@protonmail.com>
2024-09-19 11:17:44 +0200

    reworked Mode

--- shared/src/commonMain/kotlin/de/awolf/trip/kmp/core/domain/models/Mode.kt
+++ shared/src/commonMain/kotlin/de/awolf/trip/kmp/core/domain/models/Mode.kt
@@ -1,6 +1,7 @@
 package de.awolf.trip.kmp.core.domain.models
 
 enum class Mode(val rawValue: String) {
+    FOOTPATH("Footpath"),
     TRAM("Tram"),
     CITYBUS("CityBus"),
     INTERCITYBUS("IntercityBus"),
@@ -32,23 +33,24 @@ enum class Mode(val rawValue: String) {
     }
 
     fun getIconURL(): String {
-        var identifier = rawValue
-        if (rawValue == "CityBus" || rawValue == "IntercityBus") {
-            identifier = "Bus"
-        }
-        return "https://www.dvb.de/assets/img/trans-icon/transport-" + identifier + ".svg"
+        if (this == UNKNOWN || this == FOOTPATH) return ""
+
+        val identifier = if (this == CITYBUS || this == INTERCITYBUS) "Bus" else rawValue
+
+        return "https://www.dvb.de/assets/img/trans-icon/transport-$identifier.svg"
     }
 
     fun getColorHex(): Long {
-        return when (rawValue) {
-            "Tram" -> "FFDB0031" //tram red
-            "CityBus" -> "FF005D75" //bus blue
-            "IntercityBus" -> "FF005D75" //bus blue
-            "PlusBus" -> "FFA3177E" //plusbus magenta
-            "SuburbanRailway" -> "FF00914D" //sbahn green
-            "Train" -> "FF00914D" //train green
-            "Ferry" -> "FF00A5DA" //ferry lightblue
-            "HailedSharedTaxi" -> "FFFFEC01" //alita taxi yellow
+        return when (this) {
+            FOOTPATH -> "FF888888" //gray
+            TRAM -> "FFDB0031" //tram red
+            CITYBUS -> "FF005D75" //bus blue
+            INTERCITYBUS -> "FF005D75" //bus blue
+            PLUSBUS -> "FFA3177E" //plusbus magenta
+            SUBURBANRAILWAY -> "FF00914D" //sbahn green
+            TRAIN -> "FF00914D" //train green
+            FERRY -> "FF00A5DA" //ferry lightblue
+            HAILEDSHAREDTAXI -> "FFFFEC01" //alita taxi yellow
             else -> "FF888888" //gray
         }.toLong(radix = 16)
     }
